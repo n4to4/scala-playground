db33e909cd5abdb85e81fdf99cd7fc5447815e85
n4to4 <n4to4k@gmail.com>
2025-02-09 06:29:14 +0900

    scala

--- getting-started/hello.scala
+++ getting-started/hello.scala
@@ -1,6 +1,12 @@
 //> using scala 3
+//> using dep "com.lihaoyi::os-lib:0.10.7"
 
 @main
+def countFiles(): Unit =
+  val paths = os.list(os.pwd)
+  println(paths.length)
+  for p <- paths do println(s"path: $p")
+
 def hello1(name: String): Unit =
   println(s"Hello, $name!")
 
