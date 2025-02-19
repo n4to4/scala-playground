44784647db3f0f5874514c1411e7cb3bbe95cb74
n4to4 <n4to4k@gmail.com>
2025-02-19 14:19:26 +0900

    fp-in-scala

---
+++ fp-in-scala/chapter01.scala
@@ -0,0 +1,20 @@
+//> scala 3
+
+class Cafe:
+  def buyCoffee(cc: CreditCard): Coffee =
+    val cup = Coffee()
+    cc.charge(cup.price)
+    cup
+
+class CreditCard:
+  def charge(price: Double): Unit =
+    println("charging " + price)
+
+class Coffee:
+  val price: Double = 2.0
+
+@main
+def main =
+  val cc = CreditCard()
+  val cafe = Cafe()
+  val cup = cafe.buyCoffee(cc)
