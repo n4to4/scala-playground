7328786a3c240d41aa6e8f15e9a278ac03a35a56
n4to4 <n4to4k@gmail.com>
2025-02-19 16:02:48 +0900

    fp-in-scala

--- fp-in-scala/chapter02.scala
+++ fp-in-scala/chapter02.scala
@@ -3,10 +3,13 @@ def factorial(n: Int): Int =
   else n * factorial(n - 1)
 
 def factorial2(n: Int): Int =
+  @annotation.tailrec
   def go(m: Int, acc: Int): Int =
     if m <= 1 then acc else go(m - 1, acc * m)
   go(n, 1)
 
 @main
 def main02 =
-  for n <- 1 to 10 do println(s"factorial($n) = ${factorial2(n)}")
+  for n <- 1 to 10 do
+    println(s"factorial($n)  = ${factorial(n)}")
+    println(s"factorial2($n) = ${factorial2(n)}")
